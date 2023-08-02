63865765a6b95efef091b85ca4fffc3fa3f21702
Navjot Singh <navjot_@outlook.com>
2023-08-02 19:53:15 +0530

    Added a pagination list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './Data';
+import {useState} from 'react';
+
+function GetExperienceString(experience)
+{
+  if((experience["years"] === 0) && (experience["months"] === 0))
+  {
+    return "Fresher";
+  }
+  let experienceString = "";
+  if (experience["years"] !== "0")
+  {
+    experienceString = experience["years"]+" Years";
+  }
+  if (experience["months"] !== "0")
+  {
+    if(experienceString !== "")
+      experienceString = experienceString + " ";
+    experienceString = experienceString + experience["months"] + " Months"
+  }
+  return experienceString;
+}
+
+function PositionComponent({positions}){
+  let [positionExpanded, setPositionExpanded] =  useState(false);
+
+  let positionsComponent;
+  let buttonText;
+  if ((positions.length > 1) && (!positionExpanded))
+  {
+    positionsComponent = <div>{positions[0]}</div>;
+    buttonText = "+"+ (positions.length-1) +" More";
+  }
+  else{
+    positionsComponent = positions.map(position => <div>{position}</div>)
+    buttonText = "Collapse";
+  }
+  let buttonComponent = <button onClick={() => setPositionExpanded(!positionExpanded)}>{buttonText}</button>
+  return (
+  <div>
+    {positionsComponent}
+    {buttonComponent}
+  </div>
+  );
+}
+
+function TableRow({candidate}){
+  return(
+  <tr>
+    <td>
+      <div>
+        {candidate["name"]}
+      </div>
+      <div>
+        {candidate["email"]}
+      </div>
+    </td>
+    <td>{candidate["age"]}</td>
+    <td> {candidate["phone"]}</td>
+    <td><PositionComponent positions={candidate['position']}></PositionComponent></td>
+    <td><div>{'$'+candidate["salary"]+"/year"}</div><div>Average</div></td>
+    <td>
+      <div>{GetExperienceString(candidate["experience"])}</div>
+      <div>{"Last work "+GetExperienceString(candidate["lastWork"])}</div></td>
+    <td>
+      {candidate["workTypePreference"].map(workType => <div>{workType}</div>)}
+    </td>
+  </tr>
+  );
+}
+
+function PaginationTable() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let recordsPerPage = 4;
+  let totalPages = Math.floor(data.length / recordsPerPage) + 1;
+  let lowerSlice = ((pageNumber - 1) * recordsPerPage);
+  let upperSlice = pageNumber * recordsPerPage;
+  return (
+    <div>
+      <table className='table table-hover'>
+        <tr>
+          <th>Full Name - Mail</th>
+          <th>Age</th>
+          <th>Phone Number</th>
+          <th>Position</th>
+          <th>Salary</th>
+          <th>Experience</th>
+          <th>Work Type</th>
+        </tr>
+        {data.slice(lowerSlice, upperSlice).map(candidate => <TableRow candidate={candidate}></TableRow>)}
+      </table>
+      <div>
+        <button onClick={() => {setPageNumber(pageNumber - 1)}}>Previous</button>
+        {"Page "+pageNumber+" of "+totalPages}
+        <button onClick={() => {setPageNumber(pageNumber + 1)}}>Next</button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Navjot Singh
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PaginationTable></PaginationTable>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,75 @@
+let data = [
+  {
+    "name": "Navjot Singh Rajput",
+    "image": "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+    "email": "navjot@outlook.com",
+    "age": "25",
+    "phone": "+918928207354",
+    "position": ["Senior Associate", "C++ Developer"],
+    "salary": "50000",
+    "experience": {"years":"3", "months":"0"},
+    "lastWork":{"years": "0", "months":"11"},
+    "workTypePreference": ["Remote", "Hybrid", "Freelance", "Onsite"]
+  },
+  {
+    "name": "Raavi",
+    "image": "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+    "email": "raavi@outlook.com",
+    "age": "25",
+    "phone": "+918928207354",
+    "position": ["AWS", "Azure", "C++ Developer"],
+    "salary": "50000",
+    "experience": {"years":"2", "months":"11"},
+    "lastWork":{"years": "0", "months":"11"},
+    "workTypePreference": ["Remote", "Hybrid", "Freelance", "Onsite"]
+  },
+  {
+    "name": "Swapnil",
+    "image": "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+    "email": "swapnil@outlook.com",
+    "age": "25",
+    "phone": "+918928207354",
+    "position": ["Java", "Senior Associate", "C++ Developer"],
+    "salary": "50000",
+    "experience": {"years":"3", "months":"0"},
+    "lastWork":{"years": "3", "months":"0"},
+    "workTypePreference": ["Remote", "Hybrid", "Freelance", "Onsite"]
+  },
+  {
+    "name": "Mahitha",
+    "image": "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+    "email": "mahi@outlook.com",
+    "age": "25",
+    "phone": "+918928207354",
+    "position": ["Senior Associate", "C++ Developer"],
+    "salary": "50000",
+    "experience": {"years":"3", "months":"0"},
+    "lastWork":{"years": "1", "months":"11"},
+    "workTypePreference": ["Remote", "Hybrid", "Freelance", "Onsite"]
+  },
+  {
+    "name": "Tejaswi",
+    "image": "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+    "email": "teja@outlook.com",
+    "age": "25",
+    "phone": "+918928207354",
+    "position": ["AWS", "Azure", "C++ Developer"],
+    "salary": "50000",
+    "experience": {"years":"3", "months":"0"},
+    "lastWork":{"years": "0", "months":"11"},
+    "workTypePreference": ["Remote", "Hybrid", "Freelance", "Onsite"]
+  },
+  {
+    "name": "Divya",
+    "image": "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+    "email": "divya@outlook.com",
+    "age": "25",
+    "phone": "+918928207354",
+    "position": ["Java", "Senior Associate", "C++ Developer"],
+    "salary": "50000",
+    "experience": {"years":"3", "months":"0"},
+    "lastWork":{"years": "0", "months":"11"},
+    "workTypePreference": ["Remote", "Hybrid", "Freelance", "Onsite"]
+  }
+];
+export default data;
